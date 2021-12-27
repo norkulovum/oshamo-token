18f8dc7bb3d3d657c3e71346ea4fcd9215f87c88
oXshamo <norkulovum@gmail.com>
2021-12-27 12:16:36 -0500

    updated comments

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ export const RED = 'danger'
 
 export const DECIMALS = (10**18)
 
-// Shortcut to avoid passing around web3 connection
+// Short cut to avoid passing around web3 connection
 export const ether = (wei) => {
   if(wei) {
     return(wei / DECIMALS) // 18 decimal places
